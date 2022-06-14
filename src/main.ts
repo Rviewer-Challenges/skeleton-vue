37776fda5b7e2b0c68509806fd803285e5a8d7ab
albertromera <albert.romera@getwith.io>
2022-06-14 09:20:17 +0200

    feat: hexagonal directory structure

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import App from "@/App.vue";
-import router from "@/router";
+import router from "@/services/router";
 
-createApp(App).use(createPinia()).use(router).mount("#app");
+createApp(App)
+  .use(createPinia())
+  .use(router)
+  .mount("#app");
---
+++ src/ui/Header/HeaderComponent.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="header">
+    <img width="80" src="../../assets/rviewer-logo.svg" class="rviewerLogo" alt="Rviewer logo"/>
+    <h3>
+      Welcome to the Rviewer Vue skeleton!
+    </h3>
+  </header>
+</template>
+
+<style>
+.rviewerLogo {
+  margin: 3rem 0;
+}
+
+.header {
+  align-items: center;
+  border-bottom: 1px solid #4FFAB6;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  font-size: calc(1rem + 2vmin);
+  justify-content: center;
+  margin: auto;
+  max-width: 90vw;
+  text-align: center;
+}
+
+@media (min-width: 1200px) {
+  .header {
+    max-width: 60vw;
+  }
+}
+</style>
